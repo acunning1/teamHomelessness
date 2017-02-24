6d85ab10aed34b2352d2bde3b83586825960fecb
Dan Carr <pdxdiver@users.noreply.github.com>
2017-02-23 16:31:16 -0800

    Update env.sh

--- homelessAPI/bin/env.sh
+++ homelessAPI/bin/env.sh
@@ -1,23 +1,15 @@
-#! /bin/bash
+##! /bin/bash
 # Setup Project Specfics - No Secrets
-export ECS_CLUSTER=
-export ECS_PROJECT=
-export ECS_PORT=8000
-export ECS_CLUSTER_SIZE=
-export ECS_INSTANCE_TYPE=
-export AWS_REGION=
-export AWS_KEY_PAIR=
-export DOCKER_REPO=hackoregon
+export DOCKER_REPO=pdxdiver
 export DOCKER_WEB_IMAGE=homeless-api
+export CONFIG_BUCKET=homeless-dev-config
+export PROJ_SETTINGS_DIR=homelessAPI
+export DEPLOY_TARGET=INTEGRATION
 echo "##############################"
 echo  Your Environement
 echo "##############################"
-echo ECS_CLUSTER: $ECS_CLUSTER
-echo ECS_PROJECT: $ECS_PROJECT
-echo ECS_PORT: $ECS_PORT
-echo ECS_CLUSTER_SIZE: $ECS_CLUSTER_SIZE
-echo ECS_INSTANCE_TYPE: $ECS_INSTANCE_TYPE
-echo AWS_REGION: $AWS_REGION
-echo AWS_KEY_PAIR: $AWS_KEY_PAIR
 echo DOCKER_REPO: $DOCKER_REPO
 echo DOCKER_WEB_IMAGE: $DOCKER_WEB_IMAGE
+echo CONFIG_BUCKET $CONFIG_BUCKET
+echo PROJ_SETTINGS_DIR $PROJ_SETTINGS_DIR
+echo DEPLOY_TARGET $DEPLOY_TARGET
